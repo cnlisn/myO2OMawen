81ddc7e58e5d1360e0820dd40f9c7592f19dfdad
cnlisn <cnlishan@163.com>
2020-01-04 20:53:16 +0800

    添加logback 日志配置

--- src/main/java/com/lisn/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/lisn/o2o/web/superadmin/AreaController.java
@@ -3,6 +3,8 @@ package com.lisn.o2o.web.superadmin;
 import com.lisn.o2o.entity.Area;
 import com.lisn.o2o.entity.ConstantForSuperAdmin;
 import com.lisn.o2o.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,7 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superdmin")
 public class AreaController {
+    Logger logger = LoggerFactory.getLogger(AreaController.class);
     @Autowired
     AreaService areaService;
 
@@ -24,6 +27,8 @@ public class AreaController {
     //将返回数据转换为json
     @ResponseBody
     private Map<String, Object> listArea() {
+        logger.info("====start====");
+        long startTime = System.currentTimeMillis();
         HashMap<String, Object> map = new HashMap<String, Object>();
         try {
             List<Area> list = areaService.getAreaList();
@@ -35,7 +40,10 @@ public class AreaController {
             map.put("success", false);
             map.put("errMsg", e.toString());
         }
-
+        long endTime = System.currentTimeMillis();
+        logger.debug("====costTime:[{}ms]", endTime - startTime);
+        logger.info("====end====");
+        logger.error("test error!");
         return map;
     }
 
